f883d4aec1cea30a0113e10d0257a80d158a5011
zachary-povey <64191599+zachary-povey@users.noreply.github.com>
2026-01-26 00:33:33 +0100

    Attempt to fix cursor

--- misc/cage-mpv-start.sh
+++ misc/cage-mpv-start.sh
@@ -7,4 +7,4 @@ wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
 wpctl set-volume @DEFAULT_AUDIO_SINK@ 1.0
 
 # Launch mpv
-exec mpv --idle --force-window --fs --loop-playlist
+exec mpv --idle --force-window --fs
